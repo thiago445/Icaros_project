03ad462a61f48c1afdf29064594234fb25bbb218
Thiago <thiago.magalhaes11@fatec.sp.gov.br>
2024-05-23 14:10:35 -0300

    Created Events in models and class modification

---
+++ src/main/java/com/Icaros/models/Events.java
@@ -0,0 +1,135 @@
+package com.Icaros.models;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_eventos")
+public class Events {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_EVENTO")
+	private Long id;
+	
+	@Column(name = "LOCAL_EVENTO",nullable = false)
+	private String address;
+	
+	@Column(name = "DATA_EVENTO",nullable = false)
+	private LocalDate date;
+	
+	@Column(name = "HORA",nullable = false)
+	private LocalTime time;
+	
+	@Column(name = "DESC_EVENTO")
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "ID_PRODUTOR", referencedColumnName = "ID_PRODUTOR")
+	private ProducerUser producerUser;
+	
+	public Events() {}
+
+	//Constructor
+
+	public Events(Long id, String address, LocalDate date, LocalTime time, String description,
+			ProducerUser producerUser) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.date = date;
+		this.time = time;
+		this.description = description;
+		this.producerUser = producerUser;
+	}
+	
+	//Getters in Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public ProducerUser getProducerUser() {
+		return producerUser;
+	}
+
+	public void setProducerUser(ProducerUser producerUser) {
+		this.producerUser = producerUser;
+	}
+
+	
+	// hashCode and equals
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(address, date, description, id, producerUser, time);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Events other = (Events) obj;
+		return Objects.equals(address, other.address) && Objects.equals(date, other.date)
+				&& Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(producerUser, other.producerUser) && Objects.equals(time, other.time);
+	}
+	
+	
+	
+	
+
+	
+}
--- src/main/java/com/Icaros/models/UserMusician.java
+++ src/main/java/com/Icaros/models/UserMusician.java
@@ -1,7 +1,11 @@
 package com.Icaros.models;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,8 +21,12 @@ public class UserMusician {
 	@Column(name= "ID_MUSICO")
 	private Long id;
 	
-	@Column(name = "CPF")
-	private int cpf; //porque nao foi declarado no usuario
+	@Column(name = "CPF",unique = true)
+	private String cpf; //porque nao foi declarado no usuario
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "GENERO_MUSICAL")
+	private MusicalGenre musicalGenre;
 
 	
 	@Column(name = "DESCRICAO")
@@ -27,4 +35,107 @@ public class UserMusician {
 	@OneToOne
 	@JoinColumn(name = "ID_USUARIO", referencedColumnName = "ID_USUARIO")
 	private User user;
+	
+	
+	public enum MusicalGenre{
+		Rock,
+		Sertanejo,
+		Pop,
+		Hip_Hop,
+		Jazz,
+		Blues,
+		Classical,
+		Electronic_Dance_Music,
+		Country,
+		Reggae,
+		Reggaeton,
+		Soul,
+		Funk,
+		Disco,
+		Gospel,
+		Todos
+	}
+	
+	public UserMusician() {
+		
+	}
+	
+	// Getters and Setters
+
+	public UserMusician(Long id, String cpf, MusicalGenre musicalGenre, String description, User user) {
+		super();
+		this.id = id;
+		this.cpf = cpf;
+		this.musicalGenre = musicalGenre;
+		this.description = description;
+		this.user = user;
+	}
+	
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public MusicalGenre getMusicalGenre() {
+		return musicalGenre;
+	}
+
+	public void setMusicalGenre(MusicalGenre musicalGenre) {
+		this.musicalGenre = musicalGenre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	 // hashCode and equals
+		@Override
+		public int hashCode() {
+			return Objects.hash(cpf, description, id, musicalGenre, user);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			UserMusician other = (UserMusician) obj;
+			return cpf == other.cpf && Objects.equals(description, other.description) && Objects.equals(id, other.id)
+					&& musicalGenre == other.musicalGenre && Objects.equals(user, other.user);
+		}
+	
+	
+	
+	
+	
+	
+	
 }
+
