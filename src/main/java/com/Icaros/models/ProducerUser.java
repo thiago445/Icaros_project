bf22f35c3f01db68ec0bf62a60eef6a95a8fa903
Thiago <thiago.magalhaes11@fatec.sp.gov.br>
2024-05-23 14:08:58 -0300

    Created Users in Models

---
+++ src/main/java/com/Icaros/models/ProducerUser.java
@@ -0,0 +1,111 @@
+package com.Icaros.models;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario_produtor")
+public class ProducerUser {
+	
+	@Id
+	@Column(name = "ID_PRODUTOR")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "CNPJ", unique = true)
+	private String cnpj;
+	
+	@Column(name = "NOME_FANTASIA")
+	private String fantasyName;
+	
+	@OneToOne
+	@JoinColumn(name = "ID_USUARIO", referencedColumnName = "ID_USUARIO")
+	private User user;
+	
+	public ProducerUser() {	}
+	
+	
+	//Constructor
+	public ProducerUser(Long id, String cnpj, String fantasynName, User user) {
+		super();
+		this.id = id;
+		this.cnpj = cnpj;
+		this.fantasyName = fantasynName;
+		this.user = user;
+	}
+
+	//getters in setters
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+
+	public String getFantasynName() {
+		return fantasyName;
+	}
+
+
+	public void setFantasynName(String fantasynName) {
+		this.fantasyName = fantasynName;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	// hashCode and equals
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cnpj, fantasyName, id, user);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProducerUser other = (ProducerUser) obj;
+		return Objects.equals(cnpj, other.cnpj) && Objects.equals(fantasyName, other.fantasyName)
+				&& Objects.equals(id, other.id) && Objects.equals(user, other.user);
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/Icaros/models/UserLover.java
@@ -0,0 +1,89 @@
+package com.Icaros.models;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario_am")
+public class UserLover {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_AMANTE_MUSICA")
+	private Long id;
+	
+	@Column(name = "CPF")
+	private String cpf;
+	
+	@JoinColumn(name = "ID_USUARIO", referencedColumnName = "ID_USUARIO")
+	@OneToOne
+	private User user;
+
+	public UserLover(){}
+	
+	//Constructor
+
+	public UserLover(Long id, String cpf, User user) {
+		super();
+		this.id = id;
+		this.cpf = cpf;
+		this.user = user;
+	}
+	
+	//Getters in Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	// hashCode and equals
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id, user);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserLover other = (UserLover) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id) && Objects.equals(user, other.user);
+	}
+	
+	
+	
+	
+}
