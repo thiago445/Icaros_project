af88813af6414b3b9baa2e57ef919bf8fee6b939
Thiago <thiago.magalhaes11@fatec.sp.gov.br>
2024-05-26 16:52:24 -0300

    creating a connection with the page

---
+++ src/main/java/com/Icaros/configs/WebConfig.java
@@ -0,0 +1,21 @@
+package com.Icaros.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig  implements WebMvcConfigurer{
+
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+	    
+}
