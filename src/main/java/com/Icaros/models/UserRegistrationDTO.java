33f2a383d5ba3b571a4be0207ef6593c770f7fad
Thiago <thiago.magalhaes11@fatec.sp.gov.br>
2024-05-26 16:44:43 -0300

    Connection model created

---
+++ src/main/java/com/Icaros/models/UserRegistrationDTO.java
@@ -0,0 +1,48 @@
+package com.Icaros.models;
+
+public class UserRegistrationDTO{
+
+
+	private User user;
+	
+	private UserLover userLover;
+	
+	private UserMusician userMusician;
+	
+	private ProducerUser producerUser;
+
+
+	public UserMusician getUserMusician() {
+		return userMusician;
+	}
+
+	public void setUserMusician(UserMusician userMusician) {
+		this.userMusician = userMusician;
+	}
+
+	public ProducerUser getProducerUser() {
+		return producerUser;
+	}
+
+	public void setProducerUser(ProducerUser producerUser) {
+		this.producerUser = producerUser;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public UserLover getUserLover() {
+		return userLover;
+	}
+
+	public void setUserLover(UserLover userLover) {
+		this.userLover = userLover;
+	}
+	
+	
+}
